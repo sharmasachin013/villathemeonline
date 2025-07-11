2ecfa792dfaa061cc5a0162e751552d0b5ef9d6d
sharma.sachin013 <sharma.sachin013@gmail.com>
2025-07-11 10:03:21 +0530

    updated theme code

---
+++ web/themes/villaagency/js/villa.js
@@ -0,0 +1,17 @@
+// /**
+//  * @file
+//  * villa behaviors.
+//  */
+// (function (Drupal) {
+
+//   'use strict';
+
+//   Drupal.behaviors.villa = {
+//     attach (context, settings) {
+
+//       console.log('It works!');
+
+//     }
+//   };
+
+// } (Drupal));
